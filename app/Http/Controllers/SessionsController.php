00238c1a7aa53ebc1bd44bf286da85932f96c066
weilaim <879181054@qq.com>
2019-09-22 20:25:11 +0800

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class SessionsController extends Controller
+{
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create(){
+        return view('sessions.create');
+    }
+
+
+    /**
+     * @param Request $request
+     * @throws \Illuminate\Validation\ValidationException
+     *用 email 字段的值在数据库中查找；
+     *果用户被找到：
+     * 先将传参的 password 值进行哈希加密，然后与数据库中 password 字段中已加密的密码进行匹配；
+     * 如果匹配后两个值完全一致，会创建一个『会话』给通过认证的用户。会话在创建的同时，也会种下一个名为
+     *laravel_session 的 HTTP Cookie，以此 Cookie 来记录用户登录状态，最终返回 true；
+     *3). 如果匹配后两个值不一致，则返回 false；
+     *如果用户未找到，则返回 false。
+     *结合 attempt 方法对用户身份进行认证的具体代码实现如下，使用 Auth 前需要对其进行引用（注意文件顶部引入 use Auth;）：
+     *
+     */
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required|min:4'
+        ]);
+
+        //dd($credentials);
+
+        if (Auth::attempt($credentials)) {
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
+    }
+}

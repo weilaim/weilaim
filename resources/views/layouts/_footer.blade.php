f8ad85d1e8344fcac2fcf0dd4936bd9ced9717a8
weilaim <879181054@qq.com>
2019-09-22 12:54:57 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <img class="brand-icon" src="http://pic2.zhimg.com/v2-7f6357afa0e5fb40004ca559e613020f_b.jpg">
+    <a href="www.weilaim.top" target=_blank>
+        刻意练习，每日精进
+    </a>
+
+    <div class="float-right">
+        <a href="/about" >关于</a>
+    </div>
+</footer>

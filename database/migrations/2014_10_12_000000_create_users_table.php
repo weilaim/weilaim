47693a2566353c0598b875e2d62210e40d2dd36f
weilaim <879181054@qq.com>
2019-09-22 13:59:55 +0800

    Move user model to models folder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\QueryException;
 
 class CreateUsersTable extends Migration
 {

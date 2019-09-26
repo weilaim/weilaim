cb5964655117b602fe448276cf2a65ad17742f27
weilaim <879181054@qq.com>
2019-09-27 00:33:40 +0800

    更改用户资料

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
 Route::get('signup','UsersController@create')->name('signup');
+//更新用户
+Route::get('/users/{user}/edit','UsersController@edit')->name('users.edit');
 Route::resource('users', 'UsersController');
 
 
@@ -27,3 +29,4 @@ Route::resource('users', 'UsersController');
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout','SessionsController@destroy')->name('logout');
+

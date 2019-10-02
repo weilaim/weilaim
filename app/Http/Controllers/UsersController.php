2f36242f993627a08dfecf2f3a6f29da1894896b
weilaim <879181054@qq.com>
2019-10-02 22:32:18 +0800

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -40,7 +40,10 @@ class UsersController extends Controller
      * Laravel 会自动解析定义在控制器方法（变量名匹配路由片段）中的 Eloquent 模型类型声明。在上面代码中，由于 show() 方法传参时声明了类型 —— Eloquent 模型 User，对应的变量名 $user 会匹配路由片段中的 {user}，这样，Laravel 会自动注入与请求 URI 中传入的 ID 对应的用户模型实例。
      */
     public function show(User $user){
-        return view('users.show', compact('user'));
+        $statuses = $user->statuses()
+            ->orderBy('created_at','desc')
+            ->paginate(10);
+        return view('users.show', compact('user','statuses'));
     }
 
     public function store(Request $request){
@@ -132,6 +135,7 @@ class UsersController extends Controller
         session()->flash('success','删除用户成功!');
         return back();
     }
+
 
 
 

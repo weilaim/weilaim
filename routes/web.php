862b8b74c37a3004841e34d4486ee36c528dac75
weilaim <879181054@qq.com>
2019-10-02 22:47:00 +0800

    发布微博

--- routes/web.php
+++ routes/web.php
@@ -45,4 +45,6 @@ Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
 //执行密码更新操作
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
+//只创建 store，destroy
+Route::resource('statuses','StatusesController',['only' => ['store','destroy']]);
 
